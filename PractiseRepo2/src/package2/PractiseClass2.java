7a556764e2869e0a9e282869fc0d51f3a7d01845
dhanajikhandekar <120699480+dhanajikhandekar@users.noreply.github.com>
2022-12-18 10:44:40 +0530

    done

---
+++ PractiseRepo2/src/package2/PractiseClass2.java
@@ -0,0 +1,35 @@
+package package2;
+
+public class PractiseClass2 {
+
+	
+	
+	 public static void main(String args[])
+	{
+		
+		
+		System.out.println("ijijkijk");
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
